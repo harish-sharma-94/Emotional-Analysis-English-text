d2a44cd320b3c2238aa678fec3b91280f0a6c04a
Kartik <asooja@gmail.com>
2015-05-20 11:13:42 +0100

    Updated arff converter, uses enum now.

--- classifiers/src/main/java/edu/insight/mixedemotions/classifier/weka/SemevalAffectDataArffConverter.java
+++ classifiers/src/main/java/edu/insight/mixedemotions/classifier/weka/SemevalAffectDataArffConverter.java
@@ -17,20 +17,17 @@ public class SemevalAffectDataArffConverter {
 	private static String goldFilePath = "src/main/resources/Semeval  2007/trial/affectivetext_trial.emotions.gold";
 
 	public static void main(String[] args) throws IOException {	
-
 		String emotionsArff = "src/main/resources/emotionanger.arff";	
 		Map<String, Map<Emotions, Integer>> datamap = Semeval2007DataReader.readData(dataFilePath, goldFilePath);
 		ArrayList<Attribute> atts;
 		Instances data;
 		double[] vals;
-
 		for(String sentence : datamap.keySet()){
 			Map<Emotions, Integer> emotions = datamap.get(sentence);
 			Integer anger = emotions.get(Semeval2007DataReader.Emotions.ANGER);
 			System.out.println(sentence);
 			System.out.println(anger);
 		}
-
 		atts = new ArrayList<Attribute>();
 		atts.add(new Attribute("Headline", (ArrayList<String>) null));//attribute contains string values
 		atts.add(new Attribute("anger"));//added an attribute that contains numeric value
@@ -121,14 +118,11 @@ public class SemevalAffectDataArffConverter {
 				vals[6] = 0;
 			}
 			data.add(new DenseInstance(1.0, vals));
-
 		}
 		ArffSaver saver = new ArffSaver();
 		saver.setInstances(data);
 		try {
-			//saver.setFile(new File("Output/starsem2015/SWAN_34adviceCorpus.arff")); 
 			saver.setFile(new File(emotionsArff));
-			//saver.setFile(new File("Output/starsem2015/SWAN_34TestTA.arff")); 
 			saver.writeBatch();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -138,98 +132,44 @@ public class SemevalAffectDataArffConverter {
 	}
 
 	public static void arrfForClassificationMultiClass() {
-
-
 		String emotionsArff = "src/main/resources/emotionclassificationmulticlass.arff";	
 		Map<String, Map<Emotions, Integer>> datamap = Semeval2007DataReader.readData(dataFilePath, goldFilePath);
 		ArrayList<Attribute> atts;
 		ArrayList<String> attVals;
 		Instances data;
 		double[] vals;
-
 		atts = new ArrayList<Attribute>();
-		atts.add(new Attribute("Headline", (ArrayList<String>) null));//attribute contains string values
+		atts.add(new Attribute("Headline", (ArrayList<String>) null));
 		attVals = new ArrayList<String>();
-		attVals.add("anger");
-		attVals.add("disgust");
-		attVals.add("fear");
-		attVals.add("joy");
-		attVals.add("sadness");
-		attVals.add("surprise");
+		for(Emotions emotion : Emotions.values()){
+			attVals.add(emotion.name());
+		}
 		atts.add(new Attribute("label_class", attVals));
 		data = new Instances("classification file", atts, 0);
-		int max = 0;
-		int index;
 		for(String sentence : datamap.keySet()){
 			Map<Emotions, Integer> emotions = datamap.get(sentence);
-			Integer anger = emotions.get(Semeval2007DataReader.Emotions.ANGER);
-			Integer fear = emotions.get(Semeval2007DataReader.Emotions.FEAR);
-			Integer disgust = emotions.get(Semeval2007DataReader.Emotions.DISGUST);
-			Integer joy = emotions.get(Semeval2007DataReader.Emotions.JOY);
-			Integer sadness = emotions.get(Semeval2007DataReader.Emotions.SADNESS);
-			Integer surprise = emotions.get(Semeval2007DataReader.Emotions.SURPRISE);
+			Emotions maxEmotion = null;
+			int maxEmotionValue = Integer.MIN_VALUE;
+			for(Emotions emotion : emotions.keySet()){				
+				if(emotions.get(emotion)>maxEmotionValue){
+					maxEmotion = emotion;
+					maxEmotionValue = emotions.get(emotion);
+				}
+			}		
 			vals = new double[data.numAttributes()];//returns number of attributes
 			vals[0] = data.attribute(0).addStringValue(sentence);
-			max = anger;
-			index = 1;
-			if(max < fear) { 
-				max = fear;
-				index = 2;
-			}
-			if(max < disgust ) { 
-				max = disgust;
-				index = 3;
-			}
-			if(max < joy) { 
-				max = joy;
-				index = 4;
-			}
-			if(max < sadness) { 
-				max = sadness;
-				index = 5;
-			}
-			if(max < surprise) { 
-				max = surprise;
-				index = 6;
-			}
-			switch(index) {
-			case 1 :
-				vals[1] = attVals.indexOf("anger");
-				break;
-			case 2 :
-				vals[1] = attVals.indexOf("disgust");
-				break;
-			case 3 :
-				vals[1] = attVals.indexOf("fear");
-				break;
-			case 4 :
-				vals[1] = attVals.indexOf("joy");
-				break;
-			case 5 : 
-				vals[1] = attVals.indexOf("sadness");
-				break;
-			case 6:
-				vals[1] = attVals.indexOf("surprise");
-				break;
-			default:
-				vals[1] = attVals.indexOf("anger");
-				break;
-			}
+			vals[1] = attVals.indexOf(maxEmotion.name());
 			data.add(new DenseInstance(1.0, vals));
-
-		}
+		}	
 		ArffSaver saver = new ArffSaver();
 		saver.setInstances(data);
 		try {
-			//saver.setFile(new File("Output/starsem2015/SWAN_34adviceCorpus.arff")); 
 			saver.setFile(new File(emotionsArff));
-			//saver.setFile(new File("Output/starsem2015/SWAN_34TestTA.arff")); 
 			saver.writeBatch();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}			
 		System.out.println(data);
-
 	}
 }
 
